be2346bede3ec1dc2f06ffd6204bb12d810698ef
Krushna Hajare <hajarekrushna999@gmail.com>
2026-08-10 19:40:29 +0530

    Time: 0 ms (100%), Space: 46.7 MB (50.63%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next == null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow)break;
+        }
+        if(fast != slow)return null;
+        slow = head;
+        while(slow != fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
